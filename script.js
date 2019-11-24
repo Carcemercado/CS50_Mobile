db6ab6f3ac8d23e6a0efffb7a3b1377af71aa533
Carlos Arce <arce.code@gmail.com>
2019-11-24 17:54:34 -0500

    Add functionality to js
    Add checkbox input via javascript

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const classNames = {
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
-
+const deleteButton = document.getElementById('todo-delete')
 
 function newTodo() {
 
@@ -18,28 +18,43 @@ function newTodo() {
     //Create list items and append new tasks
     let listNode = document.createElement('li'); //Create li node
     let listTextNode = document.createTextNode(requestItem); //Create a text node from user input
+    //Create checkbox
+    let checkBox = document.createElement('input');
+        checkBox.type = 'checkbox';
+        checkBox.value = requestItem;
+        checkBox.name = 'todo';
+        listNode.appendChild(checkBox); //Add checkbox input prior to adding text to li
+
       listNode.appendChild(listTextNode); //append text to li
       list.appendChild(listNode); //append li to ul in html
-      listNode.classList.add(classNames.TODO_ITEM); //Add class name
-     
+      listNode.classList.add(classNames.TODO_ITEM); //Add class name for todo items
+      checkBox.classList.add(classNames.TODO_CHECKBOX); //Add class name for checkbox
+      
       alert("Success, New Item Added!");
       countPlus();
     } else{
-      alert("Please enter an item to add.")
+      alert("Please enter an item to add.");
     }
     //Create checkbox
     /*
      */
 }
-/**Delete function deleteTodo() */
+/**Delete function deleteTodo()
+ * Use check box to substract from count or on delete button minus 1 count
+ */
+
 
 let count = 1;
 function countPlus () {
   for (let i = 0; i < list.children.length; i++)
   return itemCountSpan.innerHTML = count++;
 }
+/*
 function countMinus(){
+  if(checkbox checked and ){
+    delete
+  }
   
-}
+}*/
     
 
