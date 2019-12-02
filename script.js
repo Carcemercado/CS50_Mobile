1eaf4288ff1d18053e3a5e3a39773aa9197dd4b5
Carlos Arce <arce.code@gmail.com>
2019-12-01 20:31:34 -0500

    JS Counter update
    Bug where delete button deletes UL instead of selected LI

--- script.js
+++ script.js
@@ -1,60 +1,79 @@
+"use strict";
 const classNames = {
   TODO_ITEM: 'todo-container',
   TODO_CHECKBOX: 'todo-checkbox',
   TODO_TEXT: 'todo-text',
   TODO_DELETE: 'todo-delete',
 }
-
+//Global variables
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 const deleteButton = document.getElementById('todo-delete')
+const myCheckbox = document.getElementById("checkbox");
+let count = 0;
+let checkCount = 0;
 
 function newTodo() {
-
   //Request user input
   let requestItem = prompt('Enter task for todo list...', 'New Item');
     if (requestItem != ''){
     //Create list items and append new tasks
-    let listNode = document.createElement('li'); //Create li node
+    let listItemNode = document.createElement('li'); //Create li node
     let listTextNode = document.createTextNode(requestItem); //Create a text node from user input
     //Create checkbox
     let checkBox = document.createElement('input');
         checkBox.type = 'checkbox';
+        checkBox.id = "checkbox";
         checkBox.value = requestItem;
         checkBox.name = 'todo';
-        listNode.appendChild(checkBox); //Add checkbox input prior to adding text to li
-
-      listNode.appendChild(listTextNode); //append text to li
-      list.appendChild(listNode); //append li to ul in html
-      listNode.classList.add(classNames.TODO_ITEM); //Add class name for todo items
-      checkBox.classList.add(classNames.TODO_CHECKBOX); //Add class name for checkbox
-      
+        //checkBox.addEventListener("click", checkedCounter); **********************Fix later
+    //Create button
+    let button = document.createElement("button");
+      button.innerHTML = "X";
+      button.addEventListener("click", removeTodo); // Add event listener for click event. Calls removeTodo()
+    //Append elements to listItemNode
+    listItemNode.appendChild(checkBox); //Add checkbox input prior to adding text to li
+    listItemNode.appendChild(listTextNode); //append text to li
+    listItemNode.appendChild(button);
+    //Add li with checkbox and button to ul in body
+    list.appendChild(listItemNode); //append li to ul in html
+    //Add styling via CSS class names.
+    listItemNode.classList.add(classNames.TODO_ITEM); //Add class name for todo items
+    checkBox.classList.add(classNames.TODO_CHECKBOX); //Add class name for checkbox
+    button.classList.add(classNames.TODO_DELETE); //Add class name for delete button
+    //Add to 1 count
+    (function() {
+      count++; 
+      itemCountSpan.innerHTML = count;
+      })(); //IIFE to add count anonymously
       alert("Success, New Item Added!");
-      countPlus();
     } else{
-      alert("Please enter an item to add.");
-    }
-    //Create checkbox
-    /*
-     */
+    alert("Please enter an item to add.");
+  }
 }
-/**Delete function deleteTodo()
- * Use check box to substract from count or on delete button minus 1 count
+function removeTodo(){
+  const currentItem = this;
+  count--;
+  itemCountSpan.innerHTML = count;
+  classNames.TODO_ITEM.remove(currentItem);
+}
+/** */
+function checkedCounter(){
+  uncheckedCountSpan.innerHTML = checkCount;
+  if (myCheckbox.checked == true){
+    checkCount++;
+  } else{
+    checkCount --;
+  }
+}
+/**
+ * This is the foundation for the count plus IIFE
+ * function countPlus () {
+ *   for (let i = 0; i < list.children.length; i++)
+ *   return itemCountSpan.innerHTML = count++;
+ * }
  */
 
 
-let count = 1;
-function countPlus () {
-  for (let i = 0; i < list.children.length; i++)
-  return itemCountSpan.innerHTML = count++;
-}
-/*
-function countMinus(){
-  if(checkbox checked and ){
-    delete
-  }
-  
-}*/
-    
 
